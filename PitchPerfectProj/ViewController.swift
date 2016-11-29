f15a186c4a60e6bb24206b300b769973e1e5ac89
Maria <maria@Marias-MacBook-Pro-2.local>
2016-11-30 02:31:27 +0530

    Hello World on click of record

--- PitchPerfectProj/ViewController.swift
+++ PitchPerfectProj/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Hello World")
+    }
 
+    @IBAction func stopRecording(_ sender: Any) {
+    }
 }
 
