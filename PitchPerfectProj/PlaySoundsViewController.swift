3153e4896e29a0f494ecc39c0c011b216888a655
maria <leelya85@gmail.com>
2016-12-02 19:23:08 +0530

    Complete Project

---
+++ PitchPerfectProj/PlaySoundsViewController.swift
@@ -0,0 +1,62 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfectProj
+//
+//  Created by Maria  on 12/1/16.
+//  Copyright © 2016 Maria . All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: URL!
+    
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .slow:
+            playSound(rate: 0.5)
+        case .fast:
+            playSound(rate: 1.5)
+        case .chipmunk:
+            playSound(pitch: 1000)
+        case .vader:
+            playSound(pitch: -1000)
+       
+        }
+        
+        configureUI(.playing)
+    }
+    
+    @IBAction func stopSound(_ sender: AnyObject) {
+        stopAudio()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
+    }
+    
+    
+}
